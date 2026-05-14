52e937251d40707e8d29133c82cdd6c4f7b8b4a5
karanharika <55457934+karanharika@users.noreply.github.com>
2026-05-14 15:35:14 +0530

    live now

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -7,7 +7,7 @@ import Contact from "../pages/Contact"
 
 export default function AppRouter() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/portofolio">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: "/portfolio/",
+  base: "/portofolio/",
 })
